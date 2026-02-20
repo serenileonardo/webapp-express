c973832f883149d232ae6824167ddb00096c69e7
serenileonardo <serenileonardo.web@gmail.com>
2026-02-20 18:37:44 +0100

    feat: adding show index

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server avviato su http://localhost:${port}`);
+
+app.get('/', (req, res) => {
+    res.send('Server OK');
 });
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "cinema"
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("Connesso al DB");
+});
