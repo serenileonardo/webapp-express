85508d70de66f2f9dfac4f5ec63c30182a05a640
serenileonardo <serenileonardo.web@gmail.com>
2026-02-27 00:36:14 +0100

    feat: fix path join

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
+const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/movies", movieController.index);
